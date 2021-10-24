010ef44508d4e29893c5fe7ecb7c9032b9bdf220
davidwt5 <davidwtan75@gmail.com>
2021-10-24 21:09:52 +1100

    abstracted handler code

--- src/components/ProductOverview.js
+++ src/components/ProductOverview.js
@@ -4,22 +4,25 @@ function ProductOverview(props) {
   const { id, name, img, price, quantity, addQuantity, subtractQuantity } =
     props;
 
+  function addHandler(e) {
+    console.log(Number(e.currentTarget.parentNode.dataset.id));
+    addQuantity(Number(e.currentTarget.parentNode.dataset.id));
+  }
+
+  function subtractHandler(e) {
+    subtractQuantity(Number(e.currentTarget.parentNode.dataset.id));
+  }
+
   return (
     <div className="product-overview" data-id={id}>
       <p className="name">{name}</p>
       <img src={img} alt={name} />
       <p className="price">${price}</p>
-      <button
-        type="button"
-        onClick={(e) => subtractQuantity(Number(e.currentTarget.parentNode.dataset.id))}
-      >
+      <button type="button" onClick={subtractHandler}>
         -
       </button>
-      <input type="number" value={quantity}></input>
-      <button
-        type="button"
-        onClick={(e) => addQuantity(Number(e.currentTarget.parentNode.dataset.id))}
-      >
+      <input type="number" value={quantity} readOnly></input>
+      <button type="button" onClick={addHandler}>
         +
       </button>
     </div>
