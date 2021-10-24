0852b2cc501b07b968aa04f28aa15d04ce463889
davidwt5 <davidwtan75@gmail.com>
2021-10-24 13:47:55 +1100

    Takes quantity value from prop

--- src/components/ProductOverview.js
+++ src/components/ProductOverview.js
@@ -1,14 +1,14 @@
 import "./ProductOverview.css"
 
 function ProductOverview(props) {
-  const { id, name, img, price } = props;
+  const { id, name, img, price, quantity } = props;
   return (
     <div className="product-overview" data-id={id}>
       <p className="name">{name}</p>
       <img src={img} alt={name} />
       <p className="price">${price}</p>
       <button type="button">-</button>
-      <input type="number"></input>
+      <input type="number" value={quantity}></input>
       <button type="button">+</button>
     </div>
   );
